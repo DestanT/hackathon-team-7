85d28429882636b4f9f814ba412de114ca93c9c1
Destan <destantezel@googlemail.com>
2024-04-23 17:49:57 +0100

    Register the Post model to admin panel

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
