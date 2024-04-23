81150ac365c684c4bf27eaa74897c42bded17cd8
Jack Tubby <jack200034@hotmail.com>
2024-04-23 22:13:29 +0100

    Update modles for posts

--- forum/models.py
+++ forum/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.postgres.fields import ArrayField
 
 User = get_user_model()
 
 class Post(models.Model):
     title = models.CharField(max_length=255, unique=True)
-    slug = models.SlugField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='forum_posts')
-    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='forum_posts')
+    created_on = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
+    tags = ArrayField(models.CharField(max_length=200), blank=True, default=list)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return str(self.title)
+
+# models
+# detailed view
+# list view
+# connect urls
