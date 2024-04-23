38bb7517c698e21b44529508e0808b79a93d9da7
Lydia Young <lydsjoyy@gmail.com>
2024-04-23 21:46:13 +0000

    Add Post detail view to views.py

--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic.detail import DetailView
 from .models import Post
 
-class PostList(generic.ListView):
-    queryset = Post.objects.order_by('-created_on')
-    template_name = 'index.html'
-    paginate_by = 3
+# class PostList(generic.ListView):
+#     queryset = Post.objects.order_by('-created_on')
+#     template_name = 'index.html'
+#     paginate_by = 3
+
+class PostDetailView(DetailView):
+    model = Post 
