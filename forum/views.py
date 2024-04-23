632fb654c6d1a8d7fa0a1509c599ebfd50609fd5
Destan <destantezel@googlemail.com>
2024-04-23 17:51:20 +0100

    Create initial ListView for posts

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 3
