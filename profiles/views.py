87581d87a47acebbc95ca91e1ecbb37d28c628ff
Destan <destantezel@googlemail.com>
2024-04-23 17:54:37 +0100

    Create ProfileList view using generic Django views

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Profile
 
-# Create your views here.
+class ProfileList(generic.ListView):
+    queryset = Profile.objects.order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 3
