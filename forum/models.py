bd5be5a9412d9f569170b11b40e7bec5e61ddbfc
Destan <destantezel@googlemail.com>
2024-04-24 23:20:40 +0100

    Create Comment and Tag models

--- forum/models.py
+++ forum/models.py
@@ -11,13 +11,26 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='forum_posts')
     created_on = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
-    tags = ArrayField(models.CharField(max_length=200), blank=True, default=list)
     likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return str(self.title)
+    
 
-# models
-# detailed view
-# list view
-# connect urls
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    posts = models.ManyToManyField(Post, related_name='tags', blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.content)
