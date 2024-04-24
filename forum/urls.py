fc8473f4735e09df5a3637f7bcfca93d474ac32e
Destan <destantezel@googlemail.com>
2024-04-24 21:36:55 +0100

    Add URL paths for detail view

--- forum/urls.py
+++ forum/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.PostList.as_view(), name='forum'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='thread'),
 ]
