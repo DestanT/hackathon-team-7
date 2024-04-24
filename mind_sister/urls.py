7515dd7b8fbc99c171592037bdc9771d469b9d1d
Jack Tubby <jack200034@hotmail.com>
2024-04-24 21:02:05 +0100

    Remove media

--- mind_sister/urls.py
+++ mind_sister/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('forum.urls')),
     path('profiles/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
