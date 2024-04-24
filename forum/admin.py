8696b5338aae1c2349a91f8732a51e56ed84a103
Destan <destantezel@googlemail.com>
2024-04-24 23:20:19 +0100

    Register Tag and Comment models to the admin panel

--- forum/admin.py
+++ forum/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, Tag
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_on', 'updated_at')
+    search_fields = ['post', 'author']
