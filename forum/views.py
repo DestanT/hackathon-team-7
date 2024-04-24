9df07d5b35ab4a51a84ae43ec66478524571542d
kkumyk <k.kumykova@gmail.com>
2024-04-24 10:38:53 +0100

    uncomment PostList class

--- forum/views.py
+++ forum/views.py
@@ -3,10 +3,10 @@ from django.views import generic
 from django.views.generic.detail import DetailView
 from .models import Post
 
-# class PostList(generic.ListView):
-#     queryset = Post.objects.order_by('-created_on')
-#     template_name = 'index.html'
-#     paginate_by = 3
+class PostList(generic.ListView):
+    queryset = Post.objects.order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 3
 
 class PostDetailView(DetailView):
     model = Post 
