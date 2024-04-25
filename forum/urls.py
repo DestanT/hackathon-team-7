42be4a9de5eb81a8feeaea972ee28f4c1755f4af
Jack Tubby <jack200034@hotmail.com>
2024-04-25 22:38:01 +0100

    Remove unused urls ad add the slug to see the individual post

--- forum/urls.py
+++ forum/urls.py
@@ -9,11 +9,6 @@ urlpatterns = [
     path('topics/create/', views.TopicCreateView.as_view(), name='create_topic'), # Create a new topic
     path('topics/<slug:slug>/', views.TopicDetailView.as_view(), name='topic_detail'),  # Detail view for a specific topic
     path('posts/', views.PostList.as_view(), name='forum'),  # List all posts
+    path('<slug:slug>/', views.PostDetail.as_view(), name='thread'),
     path('topics/<slug:topic_slug>/create/', views.PostCreate.as_view(), name='create_post'), # Create a new post in the topic
-    path('<slug:slug>/', views.PostDetail.as_view(), name='thread'),  # Detail view for a specific post
-    path('<slug:slug>/update/', login_required(views.PostUpdate.as_view()), name='update_thread'),  # Update a specific post
-    path('<slug:slug>/delete/', login_required(views.PostDelete.as_view()), name='delete_thread'),  # Delete a specific post
-    path('<slug:slug>/comment/', login_required(views.CommentCreate.as_view()), name='comment'),  # Add a comment to a post
-    path('<slug:slug>/comment/<int:pk>/update/', login_required(views.CommentUpdate.as_view()), name='update_comment'),  # Update a comment
-    path('<slug:slug>/comment/<int:pk>/delete/', login_required(views.CommentDelete.as_view()), name='delete_comment'),  # Delete a comment
 ]
