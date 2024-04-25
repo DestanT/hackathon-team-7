7d9249548f37c76b97945ed748ac10ded64a3931
Lydia Young <lydsjoyy@gmail.com>
2024-04-25 22:49:10 +0000

    add comment reply button to post_detail.html page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,14 @@
 /* jshint esversion: 11, jquery: true */
 
+// Button JS
+// myFunction() = document.getElementById("btn-post").addEventListener("click"); {
+    
+// const newComment = document.getElementById("new-comment").value;
+// const commentContainer = document.getElementById("comment-container");
+// const commentElement = document.createElement("p");
 
+// commentElement.innerText = newComment;
+// commentContainer.appendChild(commentElement);
+// document.getElementById("new-comment").value = "";
+// }
+// ; 
