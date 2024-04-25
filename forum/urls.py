5467bba83f59c5ff5a01aa4f51e58b630cec58ed
Jack Tubby <jack200034@hotmail.com>
2024-04-25 23:20:40 +0100

    Add edit and delete for posts & remove unused urls

--- forum/urls.py
+++ forum/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('', views.TopicListView.as_view(), name='topic_list'),  # List of all topics
     path('topics/create/', views.TopicCreateView.as_view(), name='create_topic'), # Create a new topic
     path('topics/<slug:slug>/', views.TopicDetailView.as_view(), name='topic_detail'),  # Detail view for a specific topic
-    path('posts/', views.PostList.as_view(), name='forum'),  # List all posts
-    path('<slug:slug>/', views.PostDetail.as_view(), name='thread'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='thread'), # Details of individual post
     path('topics/<slug:topic_slug>/create/', views.PostCreate.as_view(), name='create_post'), # Create a new post in the topic
+    path('posts/<slug:slug>/update/', views.PostUpdate.as_view(), name='update_post'), # Update a post
+    path('posts/<slug:slug>/delete/', views.PostDelete.as_view(), name='delete_post'), # Delete a post
 ]
