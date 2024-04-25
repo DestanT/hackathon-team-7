8b3f83a42153697d6aa6ced2b7ec0047d3a935c4
Destan <destantezel@googlemail.com>
2024-04-25 17:52:22 +0100

    Linked all URLs to crud functions

--- forum/urls.py
+++ forum/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='forum'),
+    path('new/', login_required(views.PostCreate.as_view()), name='new_thread'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='thread'),
+    path('<slug:slug>/update/', login_required(views.PostUpdate.as_view()), name='update_thread'),
+    path('<slug:slug>/delete/', login_required(views.PostDelete.as_view()), name='delete_thread'),
+    path('<slug:slug>/comment/', login_required(views.CommentCreate.as_view()), name='comment'),
+    path('<slug:slug>/comment/<int:pk>/update/', login_required(views.CommentUpdate.as_view()), name='update_comment'),
+    path('<slug:slug>/comment/<int:pk>/delete/', login_required(views.CommentDelete.as_view()), name='delete_comment'),
 ]
