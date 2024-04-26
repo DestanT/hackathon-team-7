2ca218497eb8953e9dd66ee95ef4b278b3c4634a
Destan <destantezel@googlemail.com>
2024-04-26 14:29:55 +0100

    Add app_name and update path

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'profiles'
 urlpatterns = [
-    path('', views.ProfileList.as_view(), name='profile_list'),
+    path('<int:pk>/', views.ProfileDetail.as_view(), name='profile_detail'),
+    path('<int:pk>/update', views.ProfileUpdate.as_view(), name='profile_update'),
 ]
